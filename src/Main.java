5cb2c175cac8ca0e0daca7b929fc65176e1f78c1
Reet2312 <reet.kathuria23@gmail.com>
2026-03-03 01:30:02 +0530

    UC6.queue stack

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,50 @@
 import java.util.*;
 public class PalindromeCheckerApp {
-    /*
-    Application entry Point
 
-    This is the first method executed by the JVM
-    when the program starts
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Input text : ");
-@@ -16,16 +12,22 @@ public static void main(String[] args) {
-            if(input.charAt(i)!= input.charAt(j)){
+        String input = sc.nextLine();
+        char arr[] = input.toCharArray();
+        int n = input.length();
+        boolean status = true;
+        for(int i = 0 ; i < n/2 ; i++){
+            if(arr[i] != arr[n - i - 1]){
                 status = false;
             }
+            Stack<Character> st = new Stack<>();
+            for(int i = 0 ; i < input.length();i++){
+                st.push(input.charAt(i));
+                Stack<Character> st = new Stack<>();
+                for(int i = 0 ; i < input.length();i++){
+                    st.push(input.charAt(i));
+                    Queue<Character> q = new LinkedList<>();
+                    for(char c : input.toCharArray()){
+                        q.add(c);
+                        st.push(c);
+                    }
+//        System.out.println(st);
+                    StringBuilder sb = new StringBuilder();
+                    @@ -26,12 +23,15 @@
+                            System.out.println("Is Palindrome? true");
+                }else{
+                    System.out.println("Is Palindrome? false");
+                }
+
+                System.out.println("Is Palindrom: " + status);
+
+                boolean palindrome = true;
+                while(q.size()>0){
+                    if(q.poll() != st.pop()){
+                        palindrome = false;
+                        break;
+                    }
+                }
+                System.out.println("Is Palindrom? :" +palindrome);
+
+
+
+            }
         }
-            StringBuilder sb = new StringBuilder();
-            for(int i = input.length() - 1; i >= 0 ; i--){
-                sb.append(input.charAt(i));
-            }
-            String reverse = sb.toString();
-
-            if(input.equals(reverse)){
-                System.out.println("Is Palindrome ? true "  );
-            }else{
-                System.out.println("Is Palindrome ? false "  );
-            }
-
-    @author Developer
-    @version 1.0
-     */
-        System.out.println("Is palindrome ? " + status);
-
-    public static  void main(String []args){
-        System.out.println("Welcome to the Palindrom Checker");
-        System.out.println("Version 1.0");
-        System.out.println("System initialised succesfully");
-
-        System.out.println("Is palindrome ? " + status);
-
     }
-}
 }
